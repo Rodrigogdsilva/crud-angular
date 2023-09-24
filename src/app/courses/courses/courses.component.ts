4d9b1e5b05db517cd692b8854193cacf4a43ed41
Rodrigo Gonçalves <rodrigo.g.459@gmail.com>
2023-09-24 16:29:07 -0300

    Criando o módulo app-materials para comportar todas os imports do angular-materials e exportar para os módulos necessários

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -7,6 +7,7 @@ import { Course } from '../model/course';
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
+
 export class CoursesComponent implements OnInit {
 
   courses: Course[] = [
