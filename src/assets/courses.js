1f3282e7171efb552855016ef9cf94613bdec333
Rodrigo Gonçalves <rodrigo.g.459@gmail.com>
2023-09-25 09:36:33 -0300

    Incluindo ícones nas listas de categorias via pipe

--- src/assets/courses.js
+++ src/assets/courses.js
@@ -1,3 +1,4 @@
 [
-  {"_id": "1", "nome": "Angular", "categoria": "Front-end"}
+  {"_id": "1", "nome": "Angular", "categoria": "Front-end"},
+  {"_id": "2", "nome": "Java", "categoria": "Back-end"}
 ]
